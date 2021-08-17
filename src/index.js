b2a975f833870f999fcc06f60e2b33fee46ef220
EmilyRight <emilya.korkunova@gmail.com>
2021-08-17 22:27:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined) return 0;
+  return (array.length? (array.sort((a, b) => a - b))[0]: 0);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined) return 0;
+  return (array.length? (array.sort((a, b) => b - a))[0]: 0);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined) return 0;
+  return (array.length? ((array.reduce((a, b) => a + b)) / array.length): 0);
 }
